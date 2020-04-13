e0409c5a5354c55aef924d574f3b1d068f26bade
Tejeswar Sahu <tsahu@altimetrik.com>
2020-04-14 01:22:41 +0530

    a big change made

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -9,7 +9,8 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World";
+		//return "Hello World";
+		return "Hello World : V3";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
